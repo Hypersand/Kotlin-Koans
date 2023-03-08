f9cfddb6b31a63238e1153959b0510f8a90b4aec
Hypersand <on8214@gmail.com>
2023-03-08 22:18:39 +0900

    코틀린은 메서드 오버로딩이 번거롭지 않다

---
+++ Introduction/Default arguments/src/Task.kt
@@ -0,0 +1,14 @@
+//toUpperCase : 영어 대문자로 변경합니다
+//toLowerCase : 영어 소문자로 변경합니다
+
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
+        (if (toUpperCase) name.uppercase() else name) + number
+
+fun useFoo() = listOf(
+        foo("a"),
+        foo("b", number = 1),
+        foo("c", toUpperCase = true),
+        foo(name = "d", number = 2, toUpperCase = true)
+)
+
+
